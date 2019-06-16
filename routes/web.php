b683b55175baa0ae930221bd39e0acced330a1ee
tomasszu <tomasszu@inbox.lv>
2019-06-17 00:13:34 +0300

    otrais commit

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,34 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\message;
+use App\management_post;
 
 Route::get('/', function () {
-    return view('welcome');
+
+   return view('main.welcome');
 });
+
+Route::get('/about',function()
+{
+   return view('main.about');
+});
+
+Route::get('/profile','profileController@index');
+
+Route::get('/announcements','announcementsController@index');
+
+Route::get('/messages','messagesController@index');
+
+Route::get('/messages/{id}','messagesController@show');
+
+Route::get('/forums','ForumController@index');
+
+Route::get('/forums/create','ForumController@forumcreate');
+
+Route::get('/forums/{id}','ForumController@show');
+
+Route::get('/forums/{id}/create','ForumController@postcreate');
+
+Route::post('/forums','ForumController@store_forum');
+
+Route::post('/forums/{id}','ForumController@store_post');
